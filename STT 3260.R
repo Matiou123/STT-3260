ad13c835c25e81a92b9e50fdb18c29ad98f6da96
Matiou123 <mathieulemire02@gmail.com>
2025-09-14 14:15:56 -0400

    Update STT 3260.R

--- STT 3260.R
+++ STT 3260.R
@@ -1,38 +1,85 @@
-fonction_cumul <- function(loi, t) {
-  return(loi(t))
+fonction_cumul <- function(loi, t, ...) {
+  return(loi(t, ...))
 }
 
-plot_fonction_cumul <- function(loi, intervalle,type="l") {
-  F <- fonction_cumul(loi, intervalle)
+plot_fonction_cumul <- function(loi, intervalle, type = "l", ...) {
+  # Récupérer les paramètres supplémentaires
+  params <- list(...)
   
+  # Calcul de la fonction de répartition
+  F <- fonction_cumul(loi, intervalle, ...)
+  
+  # Construire le titre dynamique
+  titre <- paste("Fonction de répartition de",
+                 deparse(substitute(loi)),
+                 if (length(params) > 0) paste0("(",
+                                                paste(names(params), params, sep = "=", collapse = ", "),
+                                                ")"))
+  
+  # Tracé
   plot(intervalle, F, type = type, col = "magenta", lwd = 3,
-       main = paste("Fonction de répartition de", deparse(substitute(loi))),
-       xlab = "t", ylab = "F(t)", ylim=c(0,1))
+       main = titre,
+       xlab = "t", ylab = "F(t)", ylim = c(0, 1))
 }
 
-fonction_survie <- function(loi, t){
-  return (1 - fonction_cumul(loi,t))
+
+fonction_survie <- function(loi, t, ...) {
+  return(1 - fonction_cumul(loi, t, ...))
 }
 
-plot_fonction_survie <- function(loi, intervalle, type="l") {
-  S <- fonction_survie(loi, intervalle)
+plot_fonction_survie <- function(loi, intervalle, type = "l", ...) {
+  # Récupère les paramètres supplémentaires
+  params <- list(...)
   
+  # Calcule la survie
+  S <- fonction_survie(loi, intervalle, ...)
+  
+  # Construit le titre
+  titre <- paste("Fonction de survie de", 
+                 deparse(substitute(loi)),
+                 if (length(params) > 0) paste0("(", 
+                                                paste(names(params), params, sep="=", collapse=", "), 
+                                                ")"))
+  
+  # Trace le graphe
   plot(intervalle, S, type = type, col = "magenta", lwd = 3,
-       main = paste("Fonction de survie de", deparse(substitute(loi))),
-       xlab = "t", ylab = "S(t)", ylim=c(0,1))
+       main = titre,
+       xlab = "t", ylab = "S(t)", ylim = c(0, 1))
 }
 
 
 # Fonction cumul d'une binomial(4,0.5)
 t <- seq(0, 4, 1)
-plot_fonction_cumul(function(x) pbinom(x, size = 4, prob = 0.5), t,type="S")
-plot_fonction_survie(function(x) pbinom(x, size = 4, prob = 0.5), t,type="S")
+plot_fonction_cumul(pbinom, t,type="S", size = 4, prob = 0.5)
+plot_fonction_survie(pbinom, t,type="S", size = 4, prob = 0.5)
 
 # Loi exponentielle de paramètre 1
-t <- seq(0, 5, 0.1)
+t <- seq(0, 5, 0.001)
 plot_fonction_cumul(pexp, t)
 plot_fonction_survie(pexp, t)
 
 # Loi Weibull shape = alpha, scale = lambda
 t <- seq(0,2,0.01)
-plot_fonction_survie(function(x) pweibull(x,10,0.5),t)
+plot_fonction_survie(pweibull,t, shape=1, scale=0.1)
+
+
+
+# Loi normale centrée réduite
+t <- seq(-3, 3, 0.01)
+plot_fonction_survie(pnorm, t)
+
+# Loi exponentielle λ=2
+t <- seq(0, 3, 0.01)
+plot_fonction_survie(pexp, t, rate = 2)
+
+# Loi Weibull α=1, λ=0.1
+t <- seq(0, 2, 0.01)
+plot_fonction_survie(pweibull, t, shape = 1, scale = 0.1)
+
+# Loi Weibull α=0.5, λ=0.27
+t <- seq(0, 2, 0.01)
+plot_fonction_survie(pweibull, t, shape = 1, scale = 0.1)
+
+# Loi Weibull α=3, λ=0.002
+t <- seq(0, 2, 0.01)
+plot_fonction_survie(pweibull, t, shape = 1, scale = 0.1)
