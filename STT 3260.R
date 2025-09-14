96bb58707299fcb16a5c4c63a497b1e6aff3a06b
Matiou123 <mathieulemire02@gmail.com>
2025-09-14 13:55:36 -0400

    Update STT 3260.R

--- STT 3260.R
+++ STT 3260.R
@@ -14,7 +14,7 @@ fonction_survie <- function(loi, t){
   return (1 - fonction_cumul(loi,t))
 }
 
-plot_fonction_cumul <- function(loi, intervalle,type="l") {
+plot_fonction_survie <- function(loi, intervalle, type="l") {
   S <- fonction_survie(loi, intervalle)
   
   plot(intervalle, S, type = type, col = "magenta", lwd = 3,
@@ -23,11 +23,12 @@ plot_fonction_cumul <- function(loi, intervalle,type="l") {
 }
 
 
-# Loi normale centrée réduite
+# Fonction cumul d'une binomial(4,0.5)
 t <- seq(0, 4, 1)
 plot_fonction_cumul(function(x) pbinom(x, size = 4, prob = 0.5), t,type="S")
-
+plot_fonction_survie(function(x) pbinom(x, size = 4, prob = 0.5), t,type="S")
 # Loi exponentielle de paramètre 1
 t <- seq(0, 5, 0.1)
 plot_fonction_cumul(pexp, t)
+plot_fonction_survie(pexp, t)
 
